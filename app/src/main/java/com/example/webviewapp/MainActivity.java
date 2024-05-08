5d3f3f6cb99ac71a92dc9acacdf5ef048d934e73
a22husba <a22husba@student.his.se>
2024-05-08 15:41:13 +0200

    Created a WebViewClient.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+
+
+
+
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
@@ -24,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        myWebView = findViewById(R.id.my_webview);
+
+        myWebView.setWebViewClient (new WebViewClient(){
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                return false;
+            }
+
+        });
 
         /*
         * Rename your App. Tip: Values->Strings
